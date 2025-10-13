c4f16fd889ebc3339dba6c5a7b0fef086823fa15
MUTHURAJ R <sathishraj146@gmail.com>
2025-10-13 19:42:50 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket-3tierapp"  # replace with your bucket name
-    key    = "ha-ecommerce-platform/terraform.tfstate"
+    bucket = "3tierappaws"  # replace with your bucket name
+    key    = "backend/terraform.tfstate"
     region = "ap-south-1"
   }
 }
