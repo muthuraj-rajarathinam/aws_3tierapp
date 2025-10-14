449ec7b932a18e0b340bd562c605189ce3f2c51a
MUTHURAJ R <sathishraj146@gmail.com>
2025-10-14 14:32:05 +0530

    Update backend.tf

--- terra/backend.tf
+++ terra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket-3tierapp"  # replace with your bucket name
+    bucket = "3tierappaws"  # replace with your bucket name
     key    = "backend/terraform.tfstate"
     region = "ap-south-1"
   }
